7a328c2f7654e3c868687887b1f90e6c4738d419
emil <emil@ajduk.com>
2011-10-08 20:23:15 +0200

    kontakt strana

--- app/views/home/_static/contactView.php
+++ app/views/home/_static/contactView.php
@@ -1,2 +1,103 @@
-#contact
+<div class="main">
+    <div class="mainBox">
+        <div class="boxTitle">
+            <h1>Kontakt</h1>
+        </div>
+        <div class="contact">
+            <div class="contactInfo">
+                <p>
+                    Nam adipiscing, lacus id congue auctor, sem augue sodales nisl, nec egestas quam lorem et odio. Nulla facilisi.
+                </p>
+            </div>
+            <div class="contactForm">
+                <form>
+                <table cellpadding="0" cellspacing="0" width="0">
+                    <tr>
+                        <td width="100" align="right">
+                            <label>Naslov Poruke</label>
+                        </td>
+                        <td>
+                            <input type="text" name="" value="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <label>Ime i prezime</label>
+                        </td>
+                        <td>
+                            <input type="text" name="" value="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <label>Firma</label>
+                        </td>
+                        <td>
+                            <input type="text" name="" value="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <label>Kontakt email</label>
+                        </td>
+                        <td>
+                            <input type="text" name="" value="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <label>Telefon</label>
+                        </td>
+                        <td>
+                            <input type="text" name="" value="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right" valign="top">
+                            <label>Poruka</label>
+                        </td>
+                        <td>
+                            <textarea name="" rows="4" cols="20"></textarea>
+                        </td>
+                    </tr>
+                    <tr align="right">
+                        <td colspan="2"><input type="submit" value="posalji" /></td>
+                    </tr>
+                </table>
+                </form>
+            </div>
+        </div>
+        <div class="contactMap">
+            mapa ide ovde
+        </div>
+    </div>
+    <div class="mainBox">
+        <div class="boxTitle">
+            <span><img src="<?= IMAGE_PATH . 'icoMap.png'; ?>" /></span>
+            <h1>Vodic kroz Beograd</h1>
+        </div>
+        <div class="boxContent">
+            <p>
+                Proin congue varius commodo. Aliquam vel luctus tellus. Nunc tempor, lectus eu scelerisque vestibulum, urna leo vehicula justo, vel mollis orci purus sit amet augue. Vestibulum porta malesuada quam. Proin blandit velit sit amet elit euismod pretium. Suspendisse elit elit, consectetur et condimentum a, blandit quis sem. Morbi ut justo tortor. 
+            </p>
+        </div>
+    </div>
+</div>
+<div class="sidebarR">
+    <ul class="banners">
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+    </ul>
+</div>
+<? print_r($params); ?>
 <? print_r($params);?>
