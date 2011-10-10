130405aa6ee7313e3b773dfcfca21cd23a38dadb
emil <emil@ajduk.com>
2011-10-10 20:02:55 +0200

    calendar css

--- app/views/layout/defaultLayout.php
+++ app/views/layout/defaultLayout.php
@@ -8,17 +8,17 @@
         <meta name="Keywords" content="" />
         <meta http-equiv="X-UA-Compatible" content="IE=7" />
         <!-- Load all assets (js + css) -->
-        <?=$html->assetsJs('jquery-1.6.4.min', ASSETS_JS_PATH);?>
-        <?=$html->assetsJs('slides.min.jquery', ASSETS_JS_PATH);?>
-        <?=$html->assetsJs('app', ASSETS_JS_PATH);?>
-        <?=$html->assetsCss('default', ASSETS_CSS_PATH);?>
-        
+        <?= $html->assetsJs('jquery-1.6.4.min', ASSETS_JS_PATH); ?>
+        <?= $html->assetsJs('slides.min.jquery', ASSETS_JS_PATH); ?>
+        <?= $html->assetsJs('app', ASSETS_JS_PATH); ?>
+        <?= $html->assetsCss('default', ASSETS_CSS_PATH); ?>
+
         <!-- Load all custom js -->
-        <?=$html->js('app', JS_PATH);?>
-        <?=$html->allCustomJs(JS_PATH.'default'.DS);?>
-        
+        <?= $html->js('app', JS_PATH); ?>
+        <?= $html->allCustomJs(JS_PATH . 'default' . DS); ?>
+
         <!-- Load all custom css -->
-        <?=$html->css('default', CSS_PATH);?>
+        <?= $html->css('default', CSS_PATH); ?>
     </head>
     <body data-controller="<?= $this->_controller; ?>" data-method="<?= $this->_action; ?>">
         <body>
@@ -34,11 +34,11 @@
                         <li><a href="#" class="youtube">Youtube</a></li>
                     </ul>
                     <ul class="lang">
-                        <li><a <?=($params['lang'] == DEFAULT_LANG ? 'class="active"':'');?> href="<?=DS.DEFAULT_LANG.(isset($params['page'])?DS.$params['page']:'');?>">SR</a></li>
+                        <li><a <?= ($params['lang'] == DEFAULT_LANG ? 'class="active"' : ''); ?> href="<?= DS . DEFAULT_LANG . (isset($params['page']) ? DS . $params['page'] : ''); ?>">SR</a></li>
                         <li>/</li>
                         <li>
-                            <? $allLang = explode('|',LANG);?>
-                            <a <?=($params['lang'] == $allLang[1] ? 'class="active"':'');?> href="<?=DS.$allLang[1].(isset($params['page'])?DS.$params['page']:'');?>">EN</a>
+                            <? $allLang = explode('|', LANG); ?>
+                            <a <?= ($params['lang'] == $allLang[1] ? 'class="active"' : ''); ?> href="<?= DS . $allLang[1] . (isset($params['page']) ? DS . $params['page'] : ''); ?>">EN</a>
                         </li>
                     </ul>
                 </div>
@@ -47,15 +47,15 @@
                 <div class="header">
 
                     <ul class="mainNav">
-                        <li><a href="<?=DS.$params['lang'].DS.'about-us';?>" <?=(isset($params['page']) && 'about-us' == $params['page'] ? 'class="active"':'');?>>O nama</a></li>
-                        <li><a href="<?=DS.$params['lang'].DS.'our-clients';?>" <?=(isset($params['page']) && 'our-clients' == $params['page'] ? 'class="active"':'');?>>Naši klijenti</a></li>
-                        <li><a href="<?=DS.$params['lang'].DS.'archive';?>" <?=(isset($params['page']) && 'archive' == $params['page'] ? 'class="active"':'');?>>Arhiva izdana</a></li>
-                        <li><a href="<?=DS.$params['lang'].DS.'gallery';?>" <?=(isset($params['page']) && 'gallery' == $params['page'] ? 'class="active"':'');?>>Foto galerija</a></li>
-                        <li><a href="<?=DS.$params['lang'].DS.'ads';?>" <?=(isset($params['page']) && 'ads' == $params['page'] ? 'class="active"':'');?>>Oglašavanje</a></li>
-                        <li><a href="<?=DS.$params['lang'].DS.'contact';?>" <?=(isset($params['page']) && 'contact' == $params['page'] ? 'class="active"':'');?>>Kontakt</a></li>
+                        <li><a href="<?= DS . $params['lang'] . DS . 'about-us'; ?>" <?= (isset($params['page']) && 'about-us' == $params['page'] ? 'class="active"' : ''); ?>>O nama</a></li>
+                        <li><a href="<?= DS . $params['lang'] . DS . 'our-clients'; ?>" <?= (isset($params['page']) && 'our-clients' == $params['page'] ? 'class="active"' : ''); ?>>Naši klijenti</a></li>
+                        <li><a href="<?= DS . $params['lang'] . DS . 'archive'; ?>" <?= (isset($params['page']) && 'archive' == $params['page'] ? 'class="active"' : ''); ?>>Arhiva izdana</a></li>
+                        <li><a href="<?= DS . $params['lang'] . DS . 'gallery'; ?>" <?= (isset($params['page']) && 'gallery' == $params['page'] ? 'class="active"' : ''); ?>>Foto galerija</a></li>
+                        <li><a href="<?= DS . $params['lang'] . DS . 'ads'; ?>" <?= (isset($params['page']) && 'ads' == $params['page'] ? 'class="active"' : ''); ?>>Oglašavanje</a></li>
+                        <li><a href="<?= DS . $params['lang'] . DS . 'contact'; ?>" <?= (isset($params['page']) && 'contact' == $params['page'] ? 'class="active"' : ''); ?>>Kontakt</a></li>
                         <li><a href="#">VODIČ KROZ BEOGRAD</a></li>
                     </ul>
-                    <a href="<?=$params['lang'];?>" class="logo">
+                    <a href="<?= $params['lang']; ?>" class="logo">
                         <img src="<?= IMAGE_PATH . 'logo.png'; ?>" />
                     </a>
                     <div class="banner">
@@ -104,6 +104,9 @@
 
                                 </ul>
                             </div>
+                            <div class="boxFooter">
+                                <a href="#">Sve vesti &raquo;</a>
+                            </div>
                         </div>
                         <div class="sidebarBox">
                             <div class="boxTitle">
@@ -111,7 +114,10 @@
                             </div>
                             <div class="boxContent loader" id="calendar">
                                 <!-- Load -->
-                                <?=$calendar;?>
+                                <?= $calendar; ?>
+                            </div>
+                            <div class="boxFooter">
+                                <a href="#">Sva dešavanja &raquo;</a>
                             </div>
                         </div>
                         <div class="sidebarBox">
@@ -191,7 +197,7 @@
                 ©2011 BG Info box. All rights reserved
             </div>
             <script type="text/javascript">
-                var lang = '<?=$params['lang'];?>';
+                var lang = '<?= $params['lang']; ?>';
             </script>
         </body>
 
