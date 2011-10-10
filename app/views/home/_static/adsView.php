75c0d1fbbc43e48c687c06fa9783fb41550bcd75
emil <emil@ajduk.com>
2011-10-10 21:47:10 +0200

    ads and shit

--- app/views/home/_static/adsView.php
+++ app/views/home/_static/adsView.php
@@ -1,2 +1,152 @@
-#ads
-<? print_r($params);?>
+<div class="main">
+    <div class="mainBox">
+        <div class="boxTitle">
+            <h1>Oglasite se</h1>
+        </div>
+        <div class="ads">
+            <? if (!empty($params['sent'])): ?>
+                <? if ('success' == $params['sent']): ?>
+                    Sent ok!
+                <? else: ?>
+                    Error in sending
+                <? endif; ?>
+            <? endif; ?>
+            <ul class="boxExtra">
+                <li class="icoDoc"><a href="#">Preuzmi deklaraciju o oglashavanju</a></li>
+                <li class="infoExtra">
+                    <p>dodatne informacije o ovome shto je levod esno</p>
+                </li>
+            </ul>
+            <div class="adsForm">
+                <form action="<?= DS . $params['lang'] . DS . 'contact'; ?>">
+                    <table cellpadding="0" cellspacing="0" width="0">
+                        <tr>
+                            <td align="right">
+                                <label for="form_title">Naziv firme</label>
+                            </td>
+                            <td>
+                                <input type="text" name="form[title]" id="form_name" class="jr" value="" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <label for="form_name">Delatnost</label>
+                            </td>
+                            <td>
+                                <input type="text" name="form[name]" id="form_name" class="jr" value="" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                &nbsp;
+                            </td>
+                            <td>
+                                <b>Zelim da se reklamiram u</b>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <input type="checkbox" name="" value="ON" />
+                            </td>
+                            <td>
+                                <label for="form_name">Bg Info Box</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <input type="checkbox" name="" value="ON" />
+                            </td>
+                            <td>
+                                <label for="form_name">Bg Info Map</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <input type="checkbox" name="" value="ON" />
+                            </td>
+                            <td>
+                                <label for="form_name">Bg Info Night Map</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <input type="checkbox" name="" value="ON" />
+                            </td>
+                            <td>
+                                <label for="form_name">Belgrade in your pocket</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <input type="checkbox" name="" value="ON" />
+                            </td>
+                            <td>
+                                <label for="form_name">Novi Sad in your pocket</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <input type="checkbox" name="" value="ON" />
+                            </td>
+                            <td>
+                                <label for="form_name">Nis in your pocket</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <label for="form_company">Kontakt osoba</label>
+                            </td>
+                            <td>
+                                <input type="text" name="form[company]" id="form_company" class="jr" value="" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <label for="form_email">Email</label>
+                            </td>
+                            <td>
+                                <input type="text" name="form[email]" id="form_email" class="jr" value="" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <label for="form_phone">Telefon</label>
+                            </td>
+                            <td>
+                                <input type="text" name="form[phone]" id="form_phone" class="jr" value="" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right" valign="top">
+                                <label for="form_message">Poruka</label>
+                            </td>
+                            <td>
+                                <textarea name="form[message]" id="form_message" class="jr" rows="4" cols="20"></textarea>
+                            </td>
+                        </tr>
+                        <tr align="right">
+                            <td colspan="2"><input type="submit" name="submit" value="posalji" /></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="sidebarR">
+    <ul class="banners">
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+    </ul>
+</div>
+<? print_r($params); ?>
--- app/views/home/_static/our-clientsView.php
+++ app/views/home/_static/our-clientsView.php
@@ -1,2 +1,53 @@
-#our clients
+<div class="mainBig">
+    <div class="mainBox">
+        <div class="boxTitle">
+            <h1>Nasi klijenti</h1>
+        </div>
+        <div class="boxContent">
+            <table cellpadding="0" cellpadding="0" width="100%">
+                <thead>
+                    <tr>
+                        <th>Klijent</th>
+                        <th width="49">BG Info Box</th>
+                        <th width="49">BG Info Map</th>
+                        <th width="49">BG Info Night Map</th>
+                        <th width="49">In Your Pocket izdanje</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <h4>Naziv klijenta</h4>
+                            <p>Info ide ovde</p>
+                        </td>
+                        <td align="center"><img width="33" height="34" src="<?= IMAGE_PATH . 'yes.png'; ?>" /></td>
+                        <td align="center"></td>
+                        <td align="center"></td>
+                        <td align="center"><img width="33" height="34" src="<?= IMAGE_PATH . 'yes.png'; ?>" /></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4>Naziv klijenta</h4>
+                            <p>Info ide ovde</p>
+                        </td>
+                        <td align="center"></td>
+                        <td align="center"><img width="33" height="34" src="<?= IMAGE_PATH . 'yes.png'; ?>" /></td>
+                        <td align="center"></td>
+                        <td align="center"></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4>Naziv klijenta</h4>
+                            <p>Info ide ovde</p>
+                        </td>
+                        <td align="center"></td>
+                        <td align="center"></td>
+                        <td align="center"><img width="33" height="34" src="<?= IMAGE_PATH . 'yes.png'; ?>" /></td>
+                        <td align="center"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 <? print_r($params);?>
