cef30bc675a271bfaef3e31ef9409d8540a490b5
emil <emil@ajduk.com>
2011-10-10 22:55:04 +0200

    vesti

--- app/views/home/_static/newsView.php
+++ app/views/home/_static/newsView.php
@@ -1,35 +1,51 @@
-<? if(!empty($newsCollection)):?>
-<!-- All news -->
-<ul>
-<? foreach($newsCollection as $n):?>
-    <li>
-        Title:<?=$n['title_'.$params['lang']];?> 
-        
-        Heading: <?=$n['heading_'.$params['lang']];?>
-        
-        Content: <?=$n['content_'.$params['lang']];?>
-        
-        Date: <?=$n['created'];?>
-        
-        <? if(!empty($n['image_name'])):?>
-        Images: <?=UPLOAD_PATH.'news'.DS.$n['image_name'];?>
-        <? endif;?>
-        
-    </li>
-<? endforeach;?>
-</ul>
-<? elseif(!empty($news)):?>
-<!-- selected news -->
-    Title:<?=$news['title_'.$params['lang']];?> 
-        
-    Heading: <?=$news['heading_'.$params['lang']];?>
+<div class="main">
+    <div class="mainBox">
+        <? if (!empty($newsCollection)): ?>
+            <!-- All news -->
+            <div class="boxTitle">
+                <h2>Sve vesti</h2>
+            </div>
 
-    Content: <?=$news['content_'.$params['lang']];?>
+            <ul class="newsAll">
+                <? foreach ($newsCollection as $n): ?>
+                    <li>
+                        <h2><?= $n['title_' . $params['lang']]; ?></h2>
+                        <span class="date"><?= $n['created']; ?></span> 
+                        <p>Heading: <?= $n['heading_' . $params['lang']]; ?></p>
+                        <a href="<?= DS . $params['lang'] . DS . 'news' . DS . $n['id'] . DS . $n['title_' . $params['lang']]; ?>">
+                            <?= $_t['news.read_more'][$params['lang']]; ?>...
+                        </a>
+                    </li>
+                <? endforeach; ?>
+            </ul>
 
-    Date: <?=$news['created'];?>
-    
-    <? if(!empty($news['image_name'])):?>
-    Images: <?=UPLOAD_PATH.'news'.DS.$news['image_name'];?>
-    <? endif;?>
-    
-<? endif; ?>
+        <? elseif (!empty($news)): ?>
+            <!-- selected news -->
+            <div class="newsOne wys">
+                <h2><?= $news['title_' . $params['lang']]; ?> </h2>
+                <span class="date"><?= $news['created']; ?></span>
+                <p><?= $news['heading_' . $params['lang']]; ?></p>
+                <? if (!empty($news['image_name'])): ?>
+                    <?= UPLOAD_PATH . 'news' . DS . $news['image_name']; ?>
+                <? endif; ?>
+                <?= $news['content_' . $params['lang']]; ?>
+            </div>
+        <? endif; ?>
+    </div>
+</div>
+<div class="sidebarR">
+    <ul class="banners">
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+        <li>
+            <img src="<?= IMAGE_PATH . 'baner.jpg'; ?>" />
+        </li>
+    </ul>
+</div>
