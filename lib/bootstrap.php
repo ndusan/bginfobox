6e0bc78d813038071d147909d14362ec5f36d87a
Dusan Novakovic <ndusan@gmail.com>
2011-10-12 10:06:28 +0200

    Set basic

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
-require_once ('config' . DS . 'config.php');
+//Include config
+if (file_exists('config' . DS . 'config.php')) {
+    require_once ('config' . DS . 'config.php');
+} else {
+    require_once ('config' . DS . 'config.php.dist');
+}
 require_once ('config' . DS . 'routes.php');
 require_once ('config' . DS . 'translate.php');
 require_once ('lib' . DS . 'share.php');
