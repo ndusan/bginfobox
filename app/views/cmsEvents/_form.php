28dd9ae7d33d33f45654421662b12a273c72164f
emil <emil@ajduk.com>
2011-10-12 22:17:59 +0200

    cms add views

--- app/views/cmsEvents/_form.php
+++ app/views/cmsEvents/_form.php
@@ -1,67 +1,62 @@
+<h3>Add event :</h3>
 <div class="addContent">
-
     <form action="/cms/event/<?= $event['formAction']; ?>" method="post" enctype="multipart/form-data">
-        <table cellpadding="0" cellspacing="0" width="100%">
 
-            <tr><td><h3>Add event :</h3>
-                    <table >
-                        <tbody>
-                            <tr>
-                                <td>Title:</td>
-                                <td>
-                                    <input type="text" name="event[title_sr]" value="<?=@$event['title_sr'];?>" class="jr"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Content:</td>
-                                <td>
-                                    <textarea name="event[content_sr]" class="jr"><?=@$event['content_sr'];?></textarea>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Title en:</td>
-                                <td>
-                                    <input type="text" name="event[title_en]" value="<?=@$event['title_en'];?>"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Content en:</td>
-                                <td>
-                                    <textarea name="event[content_en]"><?=@$event['content_en'];?></textarea>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Date start:</td>
-                                <td>
-                                    <input type="text" name="event[date_start]" value="<?=@$event['date_start'];?>" class="jr datepicker"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Date end:</td>
-                                <td>
-                                    <input type="text" name="event[date_end]" value="<?=@$event['date_end'];?>" class="jr datepicker"/>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Image:</td>
-                                <td>
-                                    <input type="file" name="image" value=""/>
-                                    <? if(isset($event['id']) && !empty($event['image_name'])):?>
-                                        <a href="<?=DS.'public'.DS.'uploads'.DS.'event'.DS.$event['image_name'];?>" target="_blank"><?=$event['image_name'];?></a>
-                                        [<a href="/cms/event/delete/image/<?=$event['id'];?>">Delete</a>]
-                                    <? endif; ?>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="2" align="center">
-                                    <input type="hidden" name="event[id]" value="<?= @$event['id']; ?>" />
-                                    <input type="submit" value="Submit" name="submit" />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-            </tr>
+        <table cellpadding="0" cellspacing="0">
+            <tbody>
+                <tr>
+                    <td>Title:</td>
+                    <td>
+                        <input type="text" name="event[title_sr]" value="<?= @$event['title_sr']; ?>" class="jr"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Content:</td>
+                    <td>
+                        <textarea name="event[content_sr]" class="jr"><?= @$event['content_sr']; ?></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Title en:</td>
+                    <td>
+                        <input type="text" name="event[title_en]" value="<?= @$event['title_en']; ?>"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Content en:</td>
+                    <td>
+                        <textarea name="event[content_en]"><?= @$event['content_en']; ?></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Date start:</td>
+                    <td>
+                        <input type="text" name="event[date_start]" value="<?= @$event['date_start']; ?>" class="jr datepicker"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Date end:</td>
+                    <td>
+                        <input type="text" name="event[date_end]" value="<?= @$event['date_end']; ?>" class="jr datepicker"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Image:</td>
+                    <td>
+                        <input type="file" name="image" value=""/>
+                        <? if (isset($event['id']) && !empty($event['image_name'])): ?>
+                            <a href="<?= DS . 'public' . DS . 'uploads' . DS . 'event' . DS . $event['image_name']; ?>" target="_blank"><?= $event['image_name']; ?></a>
+                            [<a href="/cms/event/delete/image/<?= $event['id']; ?>">Delete</a>]
+                        <? endif; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <input type="hidden" name="event[id]" value="<?= @$event['id']; ?>" />
+                        <input type="submit" value="Submit" name="submit" />
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </form>
 </div>
