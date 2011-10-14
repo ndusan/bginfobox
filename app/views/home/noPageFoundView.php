2cdfce826eb5679a476b195d618b27d8f2d17f69
emil <emil@ajduk.com>
2011-10-14 23:14:28 +0200

    cms calendar and 404 page

--- app/views/home/noPageFoundView.php
+++ app/views/home/noPageFoundView.php
@@ -1,6 +1,18 @@
-<div style="position:absolute; top:50%; left:50%; margin:-250px 0 0 -150px; width:300px;">
-<img src="<?= IMAGE_PATH . '404.jpg'; ?>" />
-<center><h1>Otisli smo na skijanje na Mount Everest!</h1>
-</center>
+<div class="404page">
+    <table width="100%">
+        <tbody
+            <tr>
+                <td width="350" align="center">
+                    <img src="<?= IMAGE_PATH . '404.jpg'; ?>" />  
+                </td>
+                <td>
+                    <h2>Hmm... stranica koju tražite ne postoji</h2>
+                    <p>Kako se ovo desilo?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra nunc eget dui vehicula in molestie nulla vehicula. Etiam sit amet massa quis turpis luctus imperdiet et id erat. Duis suscipit odio a dui blandit pretium. Donec accumsan diam vel nulla cursus tempor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                    <a href="/">Vrati se na početnu stranu</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
-<? print_r($params);?>
+<? print_r($params); ?>
