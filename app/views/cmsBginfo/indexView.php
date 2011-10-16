3a1de685e4457511b13bbcbf2871e6af009e8445
emil <emil@ajduk.com>
2011-10-16 21:54:01 +0200

    tabs

--- app/views/cmsBginfo/indexView.php
+++ app/views/cmsBginfo/indexView.php
@@ -1,7 +1,3 @@
-<ul class="addTop">
-    <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
-</ul>
-
 <div class="tabs">
     <ul>
         <li><a href="#fragment-1">Box</a></li>
@@ -9,26 +5,30 @@
         <li><a href="#fragment-3">Night Map</a></li>
         <li><a href="#fragment-4">Putovanje za dvoje</a></li>
     </ul>
-    <div id="fragment-1" class="addContent">
+    <div id="fragment-1" class="addContent1">
         <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
             <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
         </ul>
         tabela
     </div>
-    <div id="fragment-2" class="addContent">
+    <div id="fragment-2" class="addContent1">
         <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
             <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
         </ul>
         tabela
     </div>
-    <div id="fragment-3" class="addContent">
+    <div id="fragment-3" class="addContent1">
         <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
             <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
         </ul>
         tabela
     </div>
-    <div id="fragment-4" class="addContent">
+    <div id="fragment-4" class="addContent1">
         <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
             <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
         </ul>
         tabela
--- app/views/cmsPockets/indexView.php
+++ app/views/cmsPockets/indexView.php
@@ -1,3 +1,42 @@
-#dynamic pages
-
 <a href="/cms/dynamic/add/1">Add</a>
+
+<div class="tabs">
+    <ul>
+        <li><a href="#fragment-1">Box</a></li>
+        <li><a href="#fragment-2">Map</a></li>
+        <li><a href="#fragment-3">Night Map</a></li>
+        <li><a href="#fragment-4">Putovanje za dvoje</a></li>
+        <li><a href="#fragment-5"><span class="cmsEdit"></span></a></li>
+    </ul>
+    <div id="fragment-1" class="addContent1">
+        <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
+            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
+        </ul>
+        tabela
+    </div>
+    <div id="fragment-2" class="addContent1">
+        <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
+            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
+        </ul>
+        tabela
+    </div>
+    <div id="fragment-3" class="addContent1">
+        <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
+            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
+        </ul>
+        tabela
+    </div>
+    <div id="fragment-4" class="addContent1">
+        <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
+            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
+        </ul>
+        tabela
+    </div>
+    <div id="fragment-5" class="addContent1">
+        tabela sa izdanjima
+    </div>
+</div>
