db867da0bbc9e372266d5816a430e1445c91140a
emil <emil@ajduk.com>
2011-10-16 21:33:35 +0200

    new layout

--- app/views/cmsBginfo/indexView.php
+++ app/views/cmsBginfo/indexView.php
@@ -1,1 +1,36 @@
-#static index
+<ul class="addTop">
+    <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
+</ul>
+
+<div class="tabs">
+    <ul>
+        <li><a href="#fragment-1">Box</a></li>
+        <li><a href="#fragment-2">Map</a></li>
+        <li><a href="#fragment-3">Night Map</a></li>
+        <li><a href="#fragment-4">Putovanje za dvoje</a></li>
+    </ul>
+    <div id="fragment-1" class="addContent">
+        <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
+        </ul>
+        tabela
+    </div>
+    <div id="fragment-2" class="addContent">
+        <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
+        </ul>
+        tabela
+    </div>
+    <div id="fragment-3" class="addContent">
+        <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
+        </ul>
+        tabela
+    </div>
+    <div id="fragment-4" class="addContent">
+        <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
+        </ul>
+        tabela
+    </div>
+</div>
