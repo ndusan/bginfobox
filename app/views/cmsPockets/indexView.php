7b8b9a3ca11be00c51282a6f7c73735eeb05a1c3
emil <emil@ajduk.com>
2011-10-16 21:55:55 +0200

    tabs

--- app/views/cmsPockets/indexView.php
+++ app/views/cmsPockets/indexView.php
@@ -1,5 +1,7 @@
 <a href="/cms/dynamic/add/1">Add</a>
-
+<ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
+        </ul>
 <div class="tabs">
     <ul>
         <li><a href="#fragment-1">Box</a></li>
@@ -10,33 +12,32 @@
     </ul>
     <div id="fragment-1" class="addContent1">
         <ul class="addTop">
-            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
             <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
         </ul>
         tabela
     </div>
     <div id="fragment-2" class="addContent1">
         <ul class="addTop">
-            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
             <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
         </ul>
         tabela
     </div>
     <div id="fragment-3" class="addContent1">
         <ul class="addTop">
-            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
             <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
         </ul>
         tabela
     </div>
     <div id="fragment-4" class="addContent1">
         <ul class="addTop">
-            <li><a class="cmsAdd" href="/cms/user/add" >Edit About</a></li>
             <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
         </ul>
         tabela
     </div>
     <div id="fragment-5" class="addContent1">
+        <ul class="addTop">
+            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj novi grad</a></li>
+        </ul>
         tabela sa izdanjima
     </div>
 </div>
