c3a613750735232846a1c06e8a888c058ef2f612
emil <emil@ajduk.com>
2011-10-16 21:57:14 +0200

    new stuff

--- app/views/cmsPockets/indexView.php
+++ app/views/cmsPockets/indexView.php
@@ -4,10 +4,9 @@
         </ul>
 <div class="tabs">
     <ul>
-        <li><a href="#fragment-1">Box</a></li>
-        <li><a href="#fragment-2">Map</a></li>
-        <li><a href="#fragment-3">Night Map</a></li>
-        <li><a href="#fragment-4">Putovanje za dvoje</a></li>
+        <li><a href="#fragment-1">Beograd</a></li>
+        <li><a href="#fragment-2">Novi Sad</a></li>
+        <li><a href="#fragment-3">Nis</a></li>
         <li><a href="#fragment-5"><span class="cmsEdit"></span></a></li>
     </ul>
     <div id="fragment-1" class="addContent1">
@@ -23,12 +22,6 @@
         tabela
     </div>
     <div id="fragment-3" class="addContent1">
-        <ul class="addTop">
-            <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
-        </ul>
-        tabela
-    </div>
-    <div id="fragment-4" class="addContent1">
         <ul class="addTop">
             <li><a class="cmsAdd" href="/cms/user/add" >Dodaj izdanje</a></li>
         </ul>
