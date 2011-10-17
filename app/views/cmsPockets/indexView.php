086d9eeb00cbad6b03ab956f45140a68b2291e99
Dusan Novakovic <ndusan@gmail.com>
2011-10-17 10:30:36 +0200

    set pocket pages

--- app/views/cmsPockets/indexView.php
+++ app/views/cmsPockets/indexView.php
@@ -1,62 +1,62 @@
+<? if(!empty($tabs)):?>
 <ul class="addTop">
-            <li><a class="cmsEdit1" href="/cms/pockets/edit/static" >Edit about content</a></li>
-        </ul>
+    <li><a class="cmsEdit1" href="/cms/pockets/edit/static" >Edit about content</a></li>
+</ul>
+<? endif;?>
 <div class="tabs">
     <ul>
-        <? $i=1;?>
         <? if(!empty($tabs)):?>
         <? foreach($tabs as $tab):?>
-        <li><a href="#fragment-<?=$i++;?>"><?=$tab['title'];?></a></li>
+        <li><a href="#fragment-<?=$i=$tab['id'];?>"><?=$tab['title'];?></a></li>
         <? endforeach;?>
         <? endif;?>
-        <li><a href="#fragment-<?=($j=$i+1);?>"><span class="cmsEdit"></span></a></li>
+        <li><a href="#fragment-<?=++$i;?>"><span class="cmsEdit"></span></a></li>
     </ul>
     <? if(!empty($tabs)):?>
-    <? $i=1;?>
     <? foreach($tabs as $tab):?>
-    <div id="fragment-<?=$i++;?>" class="addContent1">
+    <div id="fragment-<?=$i=$tab['id'];?>" class="addContent1">
         <ul class="addTop">
-            <li><a class="cmsAdd" href="/cms/pockets/edition" >Manage editions</a></li>
+            <li><a class="cmsAdd" href="/cms/pockets/add/<?=$tab['id'];?>/edition" >New editions</a></li>
         </ul>
         tabela
     </div>
     <? endforeach;?>
     <? endif;?>
     
-    <div id="fragment-<?=$j;?>" class="addContent1">
+    <div id="fragment-<?=++$i;?>" class="addContent1">
         <ul class="addTop">
-            <li><a class="cmsAdd" href="/cms/pockets/add/edition" >Add new city</a></li>
+            <li><a class="cmsAdd" href="/cms/pockets/add/city" >Add new city</a></li>
         </ul>
         
         <!-- List of all pocktes -->
-        <? if (!empty($pocketCollection)): ?>
+        <? if (!empty($tabs)): ?>
             <table cellpadding="0" cellspacing="0" border="0" class="display" id="dataTable"> 
                 <thead> 
                     <tr> 
                         <th>Title</th>
-                        <th>Heading</th>
+                        <th>Link</th>
                         <th>Created</th> 
                         <th width="100px">Action</th>
                   </tr> 
                 </thead> 
                 <tbody> 
-                    <? foreach ($pocketCollection as $p): ?>
+                    <? foreach ($tabs as $t): ?>
                         <tr> 
-                            <td><?=$news['title_sr'];?></td>
-                            <td><?=$news['heading_sr'];?></td>
-                            <td><?=$html->convertDate($news['created'], true);?></td>
+                            <td><?=$t['title'];?></td>
+                            <td><?=$t['link'];?></td>
+                            <td><?=$html->convertDate($t['created'], true);?></td>
                             <td align="center">
                                 <!--Edit-->
-                                <a title="Edit" class="cmsEdit" href="/cms/news/edit/<?= $news['id']; ?>"></a>
+                                <a title="Edit" class="cmsEdit" href="/cms/pockets/edit/<?= $t['id']; ?>/city"></a>
                                 <!--Delete-->
-                                <a title="Delete" class="jw cmsDelete" href="/cms/news/delete/<?= $news['id']; ?>"></a>
+                                <a title="Delete" class="jw cmsDelete" href="/cms/pockets/delete/<?= $t['id']; ?>/city"></a>
                             </td> 
                   </tr> 
                     <? endforeach; ?>
                 <tfoot> 
                     <tr> 
                         <th>Title</th>
-                        <th>Heading</th>
+                        <th>Link</th>
                         <th>Created</th> 
                         <th>Action</th> 
                     </tr> 
