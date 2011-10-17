6562b2e206e22ef274f5884811674e9d26d10b59
emil <emil@ajduk.com>
2011-10-17 23:25:03 +0200

    pre pull

--- app/views/home/indexView.php
+++ app/views/home/indexView.php
@@ -1,9 +1,76 @@
 <div class="main">
     <div class="mainBox">
-        <p>tri slike</p>
+        <ul class="galleryAll">
+            <li>
+                <img title="" width="170" height="240" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" />
+            </li>
+            <li>
+                <img title="" width="170" height="240" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /> 
+            </li>
+            <li>
+                <img title="" width="170" height="240" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /> 
+            </li>
+            <li>
+                <span class="moreInfo"><a href="#">Sta je Bg Info Box?</a></span>
+            </li>
+            <li>
+                <span class="moreInfo"><a href="#">Sta je Bg Info Map?</a></span>
+            </li>
+            <li>
+                <span class="moreInfo"><a href="#">Sta je Bg Info Night Map?</a></span>
+            </li>
+        </ul>
     </div>
     <div class="mainBox">
-        <p>tri slike ispod tekst</p>
+        <ul class="galleryAll">
+            <li>
+                <img title="" width="170" height="240" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" />
+            </li>
+            <li>
+                <img title="" width="170" height="240" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /> 
+            </li>
+            <li>
+                <img title="" width="170" height="240" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /> 
+            </li>
+        </ul>
+        <div class="boxContent wys">
+            <!-- ovo sve  upucava kroz efitor jer ima 1ys klasu -->
+            <h2>Saznajte vishe o asdljas</h2>
+            <p>asd asd asd asd asd asd a</p>
+        </div>
+    </div>
+    <div class="mainBox">
+        <div class="boxTitle">
+            <span><img src="<?= IMAGE_PATH . 'icoMap.png'; ?>" /></span>
+            <h1>Vodic kroz Beograd</h1>
+        </div>
+        <div class="boxContent">
+            <p>
+                Proin congue varius commodo. Aliquam vel luctus tellus. Nunc tempor, lectus eu scelerisque vestibulum, urna leo vehicula justo, vel mollis orci purus sit amet augue. Vestibulum porta malesuada quam. Proin blandit velit sit amet elit euismod pretium. Suspendisse elit elit, consectetur et condimentum a, blandit quis sem. Morbi ut justo tortor. 
+            </p>
+        </div>
+    </div>
+    <div class="mainBox">
+        <ul class="galleryAll">
+            <li>
+                <img title="" width="170" height="100" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" />
+            </li>
+            <li>
+                <img title="" width="170" height="100" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /> 
+            </li>
+            <li>
+                <img title="" width="170" height="100" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /> 
+            </li>
+            <li>
+                <span class="moreInfo"><b>Arhiva izdanja</b></span>
+            </li>
+            <li>
+                <span class="moreInfo"><b>Saradnici</b></span>
+            </li>
+            <li>
+                <span class="moreInfo"><b>Galerija</b></span>
+            </li>
+        </ul>
     </div>
 </div>
-<? include_once '_static/_banners.php';?>
+<? include_once '_static/_banners.php'; ?>
