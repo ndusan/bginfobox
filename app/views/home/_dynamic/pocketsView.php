32108aaf75556f4e4e06d08b9e4aa7cef7e803da
emil <emil@ajduk.com>
2011-10-19 00:55:52 +0200

    views

--- app/views/home/_dynamic/pocketsView.php
+++ app/views/home/_dynamic/pocketsView.php
@@ -1,1 +1,51 @@
 #pockets
+<div class="main">
+    <div class="mainBox">
+        <div class="boxTitle">
+            <h1>In your pocket vodici</h1>
+        </div>
+        <div class="boxContent">
+            <ul class="boxExtra">
+                <li class="icoPdf"><a href="#">Cenovnik oglasavanja</a></li>
+                <li class="icoMail"><a href="#">Zelite da se oglasite? <span>Posaljite nam Vase zahteve</span></a></li>
+            </ul>
+            <p>
+                Proin congue varius commodo. Aliquam vel luctus tellus. Nunc tempor, lectus eu scelerisque vestibulum, urna leo vehicula justo, vel mollis orci purus sit amet augue. Vestibulum porta malesuada quam. Proin blandit velit sit amet elit euismod pretium. Suspendisse elit elit, consectetur et condimentum a, blandit quis sem. Morbi ut justo tortor. 
+            </p>
+            <p>
+                Proin congue varius commodo. Aliquam vel luctus tellus. Nunc tempor, lectus eu scelerisque vestibulum, urna leo vehicula justo, vel mollis orci purus sit amet augue. Vestibulum porta malesuada quam. Proin blandit velit sit amet elit euismod pretium. Suspendisse elit elit, consectetur et condimentum a, blandit quis sem. Morbi ut justo tortor. 
+            </p>
+            <p>
+                Proin congue varius commodo. Aliquam vel luctus tellus. Nunc tempor, lectus eu scelerisque vestibulum, urna leo vehicula justo, vel mollis orci purus sit amet augue. Vestibulum porta malesuada quam. Proin blandit velit sit amet elit euismod pretium. Suspendisse elit elit, consectetur et condimentum a, blandit quis sem. Morbi ut justo tortor. 
+            </p>
+            <p>
+                Proin congue varius commodo. Aliquam vel luctus tellus. Nunc tempor, lectus eu scelerisque vestibulum, urna leo vehicula justo, vel mollis orci purus sit amet augue. Vestibulum porta malesuada quam. Proin blandit velit sit amet elit euismod pretium. Suspendisse elit elit, consectetur et condimentum a, blandit quis sem. Morbi ut justo tortor. 
+            </p>
+        </div>
+        <ul class="galleryAll">
+            <li>
+                <a href="#"><img title="" width="170" height="240" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /></a>
+                <span><a href="#">Preuzmi aktuelno izdanje Belgrade in your pocket vodica</a></span>
+            </li>
+            <li>
+                <a href="#"><img title="" width="170" height="240" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /></a>
+                <span><a href="#">PPreuzmi aktuelno izdanje Novi Sad in your pocket vodica</a></span>
+            </li>
+            <li>
+                <a href="#"><img title="" width="170" height="240" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /></a>
+                <span><a href="#">Preuzmi aktuelno izdanje Nis in your pocket vodica</a></span>
+            </li>
+        </ul>
+    </div>
+    <div class="mainBox">
+        <div class="boxTitle">
+            <span><img src="<?= IMAGE_PATH . 'icoMap.png'; ?>" /></span>
+            <h1>Vodic kroz Beograd</h1>
+        </div>
+        <div class="boxContent">
+            <p>
+                Proin congue varius commodo. Aliquam vel luctus tellus. Nunc tempor, lectus eu scelerisque vestibulum, urna leo vehicula justo, vel mollis orci purus sit amet augue. Vestibulum porta malesuada quam. Proin blandit velit sit amet elit euismod pretium. Suspendisse elit elit, consectetur et condimentum a, blandit quis sem. Morbi ut justo tortor. 
+            </p>
+        </div>
+    </div>
+</div>
