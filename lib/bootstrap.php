3afd7d313f9782081ccd93220bb34bcc241a3cbc
Dusan Novakovic <ndusan@gmail.com>
2011-10-18 15:34:38 +0200

    commit after conflict

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -6,9 +6,6 @@ if (file_exists('config' . DS . 'config.php')) {
     require_once ('config' . DS . 'config.php.dist');
 }
 
-//Load cache
-require_once ('lib'.DS.'cache.php');
-
 require_once ('config' . DS . 'routes.php');
 require_once ('config' . DS . 'translate.php');
 require_once ('lib' . DS . 'share.php');
