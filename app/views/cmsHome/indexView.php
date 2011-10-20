1773a51123caef2dbd0a0cf1e839767fa56359de
emil <emil@ajduk.com>
2011-10-20 21:48:23 +0200

    optimisation

--- app/views/cmsHome/indexView.php
+++ app/views/cmsHome/indexView.php
@@ -54,45 +54,6 @@
                 </li>
             </ul>
         </div>
-        <div class="statistics">
-            <ul class="chart">
-                <li>
-                    <div class="stat1" style="height:68%"></div>
-                    <div class="stat2" style="height:25%"></div>
-                    <span>PON</span>
-                </li>
-                <li>
-                    <div class="stat1" style="height:100%"></div>
-                    <div class="stat2" style="height:49%"></div>
-                    <span>UTO</span>
-                </li>
-                <li>
-                    <div class="stat1" style="height:68%"></div>
-                    <div class="stat2" style="height:35%"></div>
-                    <span>SRE</span>
-                </li>
-                <li>
-                    <div class="stat1" style="height:45%"></div>
-                    <div class="stat2" style="height:15%"></div>
-                    <span>CET</span>
-                </li>
-                <li>
-                    <div class="stat1" style="height:76%"></div>
-                    <div class="stat2" style="height:22%"></div>
-                    <span>PET</span>
-                </li>
-                <li>
-                    <div class="stat1" style="height:27%"></div>
-                    <div class="stat2" style="height:21%"></div>
-                    <span>SUB</span>
-                </li>
-                <li>
-                    <div class="stat1" style="height:55%"></div>
-                    <div class="stat2" style="height:24%"></div>
-                    <span>NED</span>
-                </li>
-            </ul>
-        </div>
     </div>
     <div id="fragment-2">
         <div class="statistics">
