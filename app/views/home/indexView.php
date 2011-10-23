9f29f5cd137a2f13887784932d57a069c32f8ef0
emil <emil@ajduk.com>
2011-10-23 23:27:33 +0200

    bottom

--- app/views/home/indexView.php
+++ app/views/home/indexView.php
@@ -1,64 +1,64 @@
 <div class="main">
     <div class="mainBox">
-        <? if(!empty($lattestStaticEditions)):?>
-        <? $tmp = array('1'=>'bginfo-box','2'=>'bginfo-map','3'=>'bginfo-night-map');?>
-        <ul class="galleryAll">
-            <? foreach($lattestStaticEditions as $lse):?>
-            <li>
-                <a href="<?=(DS . $params['lang'] . DS . $tmp[$lse['page_id']] . DS . 'gallery'); ?>" >
-                    <img title="<?=$lse['title'];?>" alt="<?=$lse['title'];?>" width="170" height="240" src="<?=(DS.'public'.DS.'uploads'.DS.'bginfo'.DS.'thumb-'.$lse['image_name']);?>" />
-                </a>
-                <span class="info moreInfo">
-                    <a href="<?=(DS . $params['lang'] . DS . $tmp[$lse['page_id']]);?>"><?=$_t['index.question'][$params['lang']]?> <?=$lse['title'];?>?</a>
-                </span>
-            </li>
-            <? endforeach; ?>
-        </ul>
-        <? endif;?>
+        <? if (!empty($lattestStaticEditions)): ?>
+            <? $tmp = array('1' => 'bginfo-box', '2' => 'bginfo-map', '3' => 'bginfo-night-map'); ?>
+            <ul class="galleryAll">
+                <? foreach ($lattestStaticEditions as $lse): ?>
+                    <li>
+                        <a href="<?= (DS . $params['lang'] . DS . $tmp[$lse['page_id']] . DS . 'gallery'); ?>" >
+                            <img title="<?= $lse['title']; ?>" alt="<?= $lse['title']; ?>" width="170" height="240" src="<?= (DS . 'public' . DS . 'uploads' . DS . 'bginfo' . DS . 'thumb-' . $lse['image_name']); ?>" />
+                        </a>
+                        <span class="info moreInfo">
+                            <a href="<?= (DS . $params['lang'] . DS . $tmp[$lse['page_id']]); ?>"><?= $_t['index.question'][$params['lang']] ?> <?= $lse['title']; ?>?</a>
+                        </span>
+                    </li>
+                <? endforeach; ?>
+            </ul>
+        <? endif; ?>
     </div>
     <div class="mainBox">
-        <? if(!empty($lattestDynamicEditions)):?>
-        <ul class="galleryAll">
-            <? foreach($lattestDynamicEditions as $lde):?>
-            <li>
-                <a href="<?=($lde['has_link']?$lde['link']:'#');?>" target="_blank">
-                    <img title="<?=$lde['title'];?>" alt="<?=$lde['title'];?>" width="170" height="240" src="<?=(DS.'public'.DS.'uploads'.DS.'pockets'.DS.'thumb-'.$lde['image_name']);?>" />
-                </a>
-            </li>
-            <? endforeach;?>
-        </ul>
-        <? endif;?>
+        <? if (!empty($lattestDynamicEditions)): ?>
+            <ul class="galleryAll">
+                <? foreach ($lattestDynamicEditions as $lde): ?>
+                    <li>
+                        <a href="<?= ($lde['has_link'] ? $lde['link'] : '#'); ?>" target="_blank">
+                            <img title="<?= $lde['title']; ?>" alt="<?= $lde['title']; ?>" width="170" height="240" src="<?= (DS . 'public' . DS . 'uploads' . DS . 'pockets' . DS . 'thumb-' . $lde['image_name']); ?>" />
+                        </a>
+                    </li>
+                <? endforeach; ?>
+            </ul>
+        <? endif; ?>
         <div class="boxContent wys">
-            <h2><?=$_t['pockets.label'][$params['lang']];?>?</h2>
-            <p><?=$pocketContent['front_content_'.$params['lang']];?> <a href="<?= DS . $params['lang'] . DS . 'pockets'; ?>">link</a></p>
+            <h2><?= $_t['pockets.label'][$params['lang']]; ?>?</h2>
+            <p><?= $pocketContent['front_content_' . $params['lang']]; ?> <a href="<?= DS . $params['lang'] . DS . 'pockets'; ?>">link</a></p>
         </div>
     </div>
-    
+
     <!-- Load belgrade guide-->
-    <? include_once VIEW_PATH.'home'.DS.'_static'.DS.'_guide.php'; ?>
-    
+    <? include_once VIEW_PATH . 'home' . DS . '_static' . DS . '_guide.php'; ?>
+
     <div class="mainBox">
         <ul class="galleryAll">
             <li>
-                <img title="" width="170" height="100" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" />
-            </li>
-            <li>
-                <img title="" width="170" height="100" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /> 
-            </li>
-            <li>
-                <img title="" width="170" height="100" src=" <?= IMAGE_PATH . 'dummy1.jpg'; ?>" /> 
-            </li>
-            <li>
+                <a href="<?= DS . $params['lang'] . DS . 'archive'; ?>">
+                    <img title="" width="170" height="100" src=" <?= IMAGE_PATH . 'arhiva.jpg'; ?>" />
+                </a>
                 <span class="info moreInfo"><b><?= $_t['moreInfo.archive'][$params['lang']]; ?></b></span>
             </li>
             <li>
+                <a href="<?= DS . $params['lang'] . DS . 'our-clients'; ?>">
+                    <img title="" width="170" height="100" src=" <?= IMAGE_PATH . 'klijenti.jpg'; ?>" /> 
+                </a>
                 <span class="info moreInfo"><b><?= $_t['moreInfo.clients'][$params['lang']]; ?></b></span>
             </li>
             <li>
+                <a href="<?= DS . $params['lang'] . DS . 'gallery'; ?>">
+                    <img title="" width="170" height="100" src=" <?= IMAGE_PATH . 'galerija.jpg'; ?>" /> 
+                </a>
                 <span class="info moreInfo"><b><?= $_t['moreInfo.gallery'][$params['lang']]; ?></b></span>
             </li>
         </ul>
     </div>
 </div>
 <!-- Load banners -->
-<? include_once VIEW_PATH.'home'.DS.'_static'.DS.'_banners.php'; ?>
+<? include_once VIEW_PATH . 'home' . DS . '_static' . DS . '_banners.php'; ?>
