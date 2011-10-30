c80f778875f75adfcdb6503d6bb225581c865219
Dusan Novakovic <ndusan@gmail.com>
2011-10-30 18:40:19 +0100

    set tracking code

--- app/views/layout/defaultLayout.php
+++ app/views/layout/defaultLayout.php
@@ -23,7 +23,6 @@
         <?= $html->css('default', CSS_PATH); ?>
     </head>
     <body data-controller="<?= $this->_controller; ?>" data-method="<?= $this->_action; ?>">
-        <body>
             <div class="topNavW">
                 <div class="topNav">
                     <ul class="soc">
@@ -258,7 +257,18 @@
             <script type="text/javascript">
                 var lang = '<?= $params['lang']; ?>';
             </script>
-        </body>
+            <script type="text/javascript">
 
+              var _gaq = _gaq || [];
+              _gaq.push(['_setAccount', 'UA-26505741-1']);
+              _gaq.push(['_trackPageview']);
+
+              (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+              })();
+
+            </script>
     </body>
 </html>
